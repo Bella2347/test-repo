30e2ead1000063c0628ffdcc7354f7e9499e5a6c
Bella Sinclair <Bella.Sinclair@skane.se>
2025-08-20 13:40:18 +0200

    Added function print_turtles()

--- ex_script.py
+++ ex_script.py
@@ -2,3 +2,11 @@ print("Hello world")
 print("First branch")
 print("Second branch")
 print("Hippopotamus")
+
+
+def print_turtles(turtles: int = 1) -> None:
+    for turtle in range(turtles):
+        print("Turtle")
+
+
+print_turtles(10)
