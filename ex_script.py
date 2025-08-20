fd4fcbdff80f36c292094963af93faec8f6ca87f
Bella Sinclair <Bella.Sinclair@skane.se>
2025-08-20 14:55:38 +0200

    Added Helmie

--- ex_script.py
+++ ex_script.py
@@ -3,7 +3,7 @@ print("First branch")
 print("Second branch")
 print("Hippopotamus")
 print("Bunny")
-
+print("Helmie")
 
 def print_turtles(turtles: int = 1) -> None:
     for turtle in range(turtles):
