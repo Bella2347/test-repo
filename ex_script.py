3a6384b57c750821188b3e2b7c88d4d811e46e45
Bella Sinclair <Bella.Sinclair@skane.se>
2026-02-12 13:26:36 +0100

    Put code in function

--- ex_script.py
+++ ex_script.py
@@ -6,16 +6,13 @@ import re
 # Unused variable
 # Trailing white spaces
 # Unnecessary white spaces
+def test():
+    string = "test string"
 
-string = "test string"
+    if (
+        "test" in string
+        and string != "very long line to exceed maximum number of characters"
+    ):
+        print(string)
 
-if (
-    "test" in string
-    and string != "very long line to exceed maximum number of characters"
-):
-    print(string)
-
-
-v = "unused variable"
-
-# change
+    v = "unused variable"
