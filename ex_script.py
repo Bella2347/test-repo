ec7205d4c5d807cc16e15ad83ce7a8721bd97320
Bella Sinclair <Bella.Sinclair@skane.se>
2026-02-26 11:30:23 +0100

    Bad format

--- ex_script.py
+++ ex_script.py
@@ -4,18 +4,15 @@
 # Unused variable
 # Trailing white spaces
 # Unnecessary white spaces
+import re
+
 def test(test: str) -> None:
     string = "test string" + test
 
-    if (
-        "test" in string
-        and string != "very long line to exceed maximum number of characters"
-    ):
-        print(string)
+    if ("test" in string and string != "new very long line to exceed maximum number of characters"):
+        print( string )
 
-    v = "unused variable"
-
-    string += v
+    v = "unused variable"   
 
 
 test("1")
